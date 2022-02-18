7afad33d3d4cfdb9b17c0cb81da2ada76b6ac213
lexfer98 <82776280+lexfer98@users.noreply.github.com>
2022-02-18 20:39:46 +0100

    Proyecto

---
+++ src/main/java/es/uji/ei1027/skillsharing/SkillSharingApplication.java
@@ -0,0 +1,21 @@
+package es.uji.ei1027.skillsharing;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class SkillSharingApplication implements CommandLineRunner {
+	private static final Logger log = Logger.getLogger(SkillSharingApplication .class.getName());
+
+	public static void main(String[] args) {
+		// Auto-configura l'aplicació
+		new SpringApplicationBuilder(SkillSharingApplication.class).run(args);
+	}
+	public void run(String... strings) throws Exception {
+		log.info("Ací va el meu codi");
+	}
+
+}
