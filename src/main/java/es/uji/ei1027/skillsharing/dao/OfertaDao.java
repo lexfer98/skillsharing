9f9b61982b7d3651855168b33d27d09048c1a372
JoanP26 <joanpenarrochacentelles6@gmail.com>
2022-03-08 10:29:46 +0100

    OfertaDao hecho

--- src/main/java/es/uji/ei1027/skillsharing/dao/OfertaDao.java
+++ src/main/java/es/uji/ei1027/skillsharing/dao/OfertaDao.java
@@ -25,14 +25,43 @@ public class OfertaDao {
         jdbcTemplate.update("INSERT INTO oferta VALUES(?, ?, ?, ?, ?, ?, ?)",
                 oferta.getIdOferta(), oferta.getIdHabilidad(), oferta.getDniPropietario(), oferta.getNombre(), oferta.getDescripcion(), oferta.getFechaIniciacion(), oferta.getFechaFinalizacion());
     }
-    public List<Oferta> getOferta() {
+    public List<Oferta> getOferta(String dniPropietario) {
         try {
-            return jdbcTemplate.query("SELECT * FROM Oferta  AND activa = true",
+            return jdbcTemplate.query("SELECT * FROM Oferta",
                     new OfertaRowMapper());
-
         }
         catch(EmptyResultDataAccessException e) {
             return new ArrayList<Oferta>();
         }
+    }
+
+    public void updateHabilidadOferta(Oferta oferta) {
+        jdbcTemplate.update("UPDATE oferta SET id_habilidad = '?' WHERE id_oferta = '?'",
+                oferta.getIdHabilidad(), oferta.getIdOferta());
+    }
+
+    public void updateNombreOferta(Oferta oferta) {
+        jdbcTemplate.update("UPDATE oferta SET nombre = '?' WHERE id_oferta = '?'",
+                oferta.getNombre(), oferta.getIdOferta());
+    }
+
+    public void updateDescripcionOferta(Oferta oferta) {
+        jdbcTemplate.update("UPDATE oferta SET descripcion = '?' WHERE id_oferta = '?'",
+                oferta.getDescripcion(), oferta.getIdOferta());
+    }
+
+    public void FechaIniciacionOferta(Oferta oferta) {
+        jdbcTemplate.update("UPDATE oferta SET fecha_inic = '?' WHERE id_oferta = '?'",
+                oferta.getFechaIniciacion(), oferta.getIdOferta());
+    }
+
+    public void FechaFinalizacionOferta(Oferta oferta) {
+        jdbcTemplate.update("UPDATE oferta SET fecha_fin = '?' WHERE id_oferta = '?'",
+                oferta.getFechaFinalizacion(), oferta.getIdOferta());
+    }
+
+    public void deleteOferta(Oferta oferta) {
+        jdbcTemplate.update("UPDATE oferta SET activa = false WHERE id_oferta = '?'",
+                oferta.getIdOferta());
     }
 }
