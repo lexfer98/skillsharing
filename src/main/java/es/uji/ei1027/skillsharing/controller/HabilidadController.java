2066e3f6d0d223c861e8ded4c6a22aec3164bf43
Sinisito <sinisito1@gmail.com>
2022-03-08 10:48:04 +0100

    HabilidadController cambiado

--- src/main/java/es/uji/ei1027/skillsharing/controller/HabilidadController.java
+++ src/main/java/es/uji/ei1027/skillsharing/controller/HabilidadController.java
@@ -1,10 +1,13 @@
 package es.uji.ei1027.skillsharing.controller;
 
 import es.uji.ei1027.skillsharing.dao.HabilidadDao;
+import es.uji.ei1027.skillsharing.model.Habilidad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/habilidad")
@@ -22,4 +25,16 @@ public class HabilidadController {
         model.addAttribute("habilidades", habilidadDao.getHabilidades());
         return "habilidad/list";
     }
+
+    @RequestMapping(value="/add")
+    public String addHabilidad(Model model) {
+        model.addAttribute("habilidad", new Habilidad());
+        return "habilidad/add";
+    }
+    @RequestMapping(value="/update/{id_habilidad}", method = RequestMethod.GET)
+    public String editHabilidad(Model model, @PathVariable String id_habilidad) {
+        model.addAttribute("habilidad", id_habilidad);
+        return "habilidad/update";
+    }
+
 }
