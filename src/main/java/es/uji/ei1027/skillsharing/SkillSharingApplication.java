a67f8b6ee3a93cd93066da50e1fec9e512254585
Bujapetas <bujapetas@gmail.com>
2022-05-08 15:44:19 +0200

    fix csv x2

--- src/main/java/es/uji/ei1027/skillsharing/SkillSharingApplication.java
+++ src/main/java/es/uji/ei1027/skillsharing/SkillSharingApplication.java
@@ -3,10 +3,12 @@ package es.uji.ei1027.skillsharing;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.logging.Logger;
-
 @SpringBootApplication
 public class SkillSharingApplication extends WebMvcConfigurationSupport implements CommandLineRunner{
 	private static final Logger log = Logger.getLogger(SkillSharingApplication .class.getName());
@@ -17,5 +19,10 @@ public class SkillSharingApplication extends WebMvcConfigurationSupport implemen
 	}
 	public void run(String... strings) throws Exception {
 		log.info("Ací va el meu codi");
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+		super.addResourceHandlers(registry);
 	}
 }
